d0e5452d85b2f5870ccabad2437412275c96effd
Abhisek Joshi <nofursad@gmail.com>
2022-03-31 16:27:56 +0000

    Correct allowed hosts in settings

--- pp4merosathi/settings.py
+++ pp4merosathi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4merosathi.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp4-merosathi.herokuapp.com', 'localhost']
 
 
 # Application definition
