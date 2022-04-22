59553b229574d61ba240a429699cffb53c5569b9
Abhisek Joshi <nofursad@gmail.com>
2022-04-22 22:16:47 +0000

    Clearing out the code

--- pp4merosathi/views.py
+++ pp4merosathi/views.py
@@ -4,11 +4,12 @@ from allauth.account.forms import LoginForm
 
 def home_page(request):
     if request.user.is_authenticated:
-        # user = request.user
-        # context = {
-        #     'user': user
-        # }
-        return redirect('userprofile/profilepage.html')
+        user = request.user
+        context = {
+            'user': user
+        }
+        # return render(request, 'userprofile/profilepage.html', context)
+        return redirect('post/main.html', context)
     else:
         form = LoginForm(request.POST or None)
         context = {
