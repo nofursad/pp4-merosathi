3e64d23b3e357d7efc9d3c129f54ffca638ea018
Abhisek Joshi <nofursad@gmail.com>
2022-04-27 07:51:40 +0000

    Update test for post model

--- post/tests_models.py
+++ post/tests_models.py
@@ -4,14 +4,17 @@ from .models import Post, Comment
 
 class TestPostModels(TestCase):
 
-    # test that message value needs to be provided
-    def test_message_is_required(self):
-        form = CommentForm({'body': ''})
-        self.assertFalse(form.is_valid())
-        self.assertIn('body', form.errors.keys())
-        self.assertEqual(form.errors['body'][0], 'This field is required.')
+    # setup test data for post and comment
+    def test_data(self):
+        self.post = Post.objects.create{
+            content="This is test content"
+            image=""
+            liked="abhisek"
+            updated_on=""
+            created_on=""
+            author="abhisek"
+        }
 
-    # test that message is named as an explicit field
-    def test_fields_are_explicit_in_forms_metaclass(self):
-        form = CommentForm()
-        self.assertEqual(form.Meta.fields, ('body',))
+    # test that message value needs to be provided
+    def test_model_str(self):
+        self.assertEqual(str(self.post), 'This is test content')
